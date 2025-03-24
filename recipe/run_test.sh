801211aa2db4dc4329e8b3b1ebd91733eb644ab3
Serhii Kupriienko <skupriienko@anaconda.com>
2025-03-24 12:07:31 +0200

    Fix test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,7 +4,7 @@ set -ex
 
 test ! -f ${PREFIX}/lib/libtiff.a
 test ! -f ${PREFIX}/lib/libtiffxx.a
-test -f ${PREFIX}/lib/libtiff{{ SHLIB_EXT }}
-test -f ${PREFIX}/lib/libtiffxx{{ SHLIB_EXT }}
+test -f ${PREFIX}/lib/libtiff${SHLIB_EXT}
+test -f ${PREFIX}/lib/libtiffxx${SHLIB_EXT}
 
 python downstream_tests.py
